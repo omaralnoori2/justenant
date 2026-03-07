34ab0d82388f039101ba30bfaf1e22450bc9c043
JusTenant Dev <dev@justenant.com>
2026-03-08 00:06:55 +0300

    Force rebuild trigger

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,6 +3,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  // Force rebuild
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api');
